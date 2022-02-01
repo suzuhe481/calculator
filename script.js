b0954601d1e84b9ffcaabbae1bbdb0bb53515c09
Hector <baseballhector@gmail.com>
2022-01-31 23:29:06 -0500

    Simple calculator styling. Added keybaord support.

--- script.js
+++ script.js
@@ -5,38 +5,56 @@ let result = 0;
 let displayToBeEmptied = true;
 let zeroError = false;
 const zeroErrorMsg = "No no no";
+let decimalExists = false;
 
 const displayWindow = document.getElementById("display");
 
 // Value of button clicked gets put on display.
 // Stores the displayed value.
 function toDisplay(e) {
+    // If display needs to be emptied.
     if(displayToBeEmptied == true) {
         emptyDisplay();
     }
-    
 
-    // Displays error
+    // Displays error.
     if(zeroError == true) {
         displayZeroErrorMsg();
 
         return;
     }
 
-    // Displays the result
+    
+    // Displays result
     else if(typeof e == "number") {
-        displayWindow.value = e;
-        displayValue = e;
-        console.log("one");
-    }
-    // Appends new number to display number
-    else {
-        displayWindow.value += e.target.textContent;
-        displayValue = displayWindow.value;
-        console.log("two");
+        // Checks if whole number or decimal.
+        if(e % 1 != 0) {
+            displayWindow.value = e;
+            displayValue = e;
+        }
+        else {
+            displayWindow.value = e;
+            displayValue = e;
+        }
     }
 
-    console.log("out");
+    // Displays decimal point.
+    else if(e == ".") {
+        displayWindow.value += e;
+        displayValue = e;
+    }
+
+    // If on screen button is pressed.
+    else if(typeof e == "object") {
+        displayWindow.value += e.target.textContent;
+        displayValue = displayWindow.value;
+    }
+    
+    // If parameter is sent.
+    else {
+        displayWindow.value += e;
+        displayValue = displayWindow.value;
+    }
 }
 
 
@@ -49,25 +67,45 @@ function storeOp(e) {
     }
     // On empty stored variable.
     else if(storedValue == 0) {
-        storedValue = displayValue;
-        storedOperator = e.target.value;
-        displayToBeEmptied = true;
+        // Keyboard press
+        if(typeof e == "string") {
+            storedValue = displayValue;
+            storedOperator = e;
+            displayToBeEmptied = true;
+        }
+        else {
+            storedValue = displayValue;
+            storedOperator = e.target.value;
+            displayToBeEmptied = true;
+        }
+        
     }
     
-    // Operating
+    // Operating without pressing equals.
     else {
-        operate();
-        storedValue = displayValue;
-        storedOperator = e.target.value;
-        displayValue = "";
-        displayToBeEmptied = true;
+        if(typeof e == "string") {
+            operate();
+            storedValue = displayValue;
+            storedOperator = e;
+            displayValue = "";
+            displayToBeEmptied = true;
+        }
+        else {
+            operate();
+            storedValue = displayValue;
+            storedOperator = e.target.value;
+            displayValue = "";
+            displayToBeEmptied = true;
+        }
     }
-
-    
 }
 
 // Evaluates expression and displays it.
 function operate() {
+    if(storedValue == 0) {
+        return;
+    }
+
     console.log(storedValue + " " + storedOperator + " " + displayValue);
 
     if(zeroError == true) {
@@ -76,17 +114,20 @@ function operate() {
 
     switch (storedOperator) {
         case "add":
+        case "+":
             result = Number(storedValue) + Number(displayValue);
             break;
         case "sub":
+        case "-":
             result = Number(storedValue) - Number(displayValue);
             break;
         case "mul":
+        case "*":
             result = Number(storedValue) * Number(displayValue);
             break;
         case "div":
+        case "/":
             if(displayValue == 0) {
-                // result = zeroErrorMsg;
                 zeroError = true;
                 break;
             }
@@ -97,17 +138,27 @@ function operate() {
             
     }
 
+    // Checks if whole number or decimal.
+    // If decimal, convert to float.
+    if(result % 1 != 0) {
+        result = parseFloat(result.toFixed(8));
+    }
+    
     storedValue = 0;
     toDisplay(result);
     displayToBeEmptied = true;
+    decimalExists = false;
 }
 
-// Empties the display and clears the data.
+// Empties the display and clears data.
 function clearData() {
     zeroError = false;
     zeroDisplay();
     storedValue = 0;
+    displayValue = 0;
+    result = 0;
     storedOperator = "";
+    decimalExists == false;
 }
 
 // Displays the zero error message.
@@ -128,19 +179,97 @@ function emptyDisplay() {
     displayToBeEmptied = false;
 }
 
+// Adds a decimal, unless one is already on screen.
+function addDecimal() {
+    if(decimalExists == true) {
+        return;
+    }
+    else {
+        toDisplay(".");
+        decimalExists = true;
+    }
+}
 
-// Adds event listeners for all numbers when clicked.
+// Lowers or resets opacity of display text when mouse hovers over solar panel.
+function lowerOpacity() {
+    displayWindow.style.color = "rgba(0, 0, 0, 0.4)";
+}
+function resetOpacity() {
+    displayWindow.style.color = "rgba(0, 0, 0, 1)";
+}
+
+// Changes the display value from positive to negative and the reverse.
+function plusMinus() {
+    displayWindow.value = (-1)*(displayWindow.value);
+    displayValue = displayWindow.value;
+}
+
+// Event listeners to add numbers to display.
 const numbers = Array.from(document.querySelectorAll(".num"));
 numbers.forEach(num => num.addEventListener("click", toDisplay));
 
-// Adds event listener for operations.
+// Event listener for operations.
 const operations = Array.from(document.querySelectorAll(".op"));
 operations.forEach(op => op.addEventListener("click", storeOp));
 
-// Evaulates the expression.
-const equals = document.getElementById("equals");
-equals.addEventListener("click", operate);
+// Event listener for equals.
+document.getElementById("equals").addEventListener("click", operate);
 
-
-// Adds an event listener for the clear button.
+// Event listener for clear button.
 document.getElementById("clear").addEventListener("click", clearData);
+
+// Event listener for decimal button.
+document.getElementById("decimal").addEventListener("click", addDecimal);
+
+document.getElementById("plus-minus").addEventListener("click", plusMinus);
+
+// Event listener for solar panel text opacity.
+document.getElementById("solar-panel").addEventListener("mouseover", lowerOpacity);
+document.getElementById("solar-panel").addEventListener("mouseout", resetOpacity);
+
+// Detects when a keyboard number is pressed.
+window.addEventListener("keydown", inputKey);
+
+function inputKey(e) {
+    let keyValue = e.key;
+
+    // Regular expression to check for the 4 characters of + - * / .
+    const opsRegex = /[\+\-\*\/]/g;
+    
+    // Regulat expressoin to check for the character of "." 
+    const decimalRegex = /[.]/g;
+
+    // If number key is pressed.
+    if(/\d/.test(keyValue)) {
+        toDisplay(keyValue);
+    }
+
+    // If operation key is pressed.
+    else if(opsRegex.test(keyValue)) {
+        storeOp(keyValue);
+    }
+
+    // If decimal key is pressed.
+    else if(decimalRegex.test(keyValue)) {
+        addDecimal();
+    }
+
+    // If Enter key is pressed.
+    else if(keyValue == "Enter") {
+        operate();
+    }
+
+    // If Delete or Backspace key is pressed.
+    else if(keyValue == "Delete" || keyValue == "Backspace") {
+        clearData();
+    }
+
+    // If , is pressed.
+    else if(keyValue == ",") {
+        plusMinus();
+    }
+
+    else {
+        return;
+    }
+}
